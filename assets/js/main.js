59489506de894d510f6f77723755adb32ae7d886
thssputhuppally <godlykmathews@outlook.com>
2022-11-26 18:56:25 +0530

    Delete unwanted

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,23 +154,6 @@
 
 
   /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function() {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
