e0fe23a943e633f765439d52ced83f3567c760c4
Godly K Mathews <godlykmathews@outlook.com>
2022-12-15 20:34:49 +0530

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,3 +177,17 @@
 $(function () {
   $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
+
+//annoucements
+    $(function () {
+        $(".demo1").bootstrapNews({
+            newsPerPage: 3,
+            autoplay: true,
+			pauseOnHover:true,
+            direction: 'up',
+            newsTickerInterval: 4000,
+            onToDo: function () {
+                //console.log(this);
+            }
+        });
+    });
