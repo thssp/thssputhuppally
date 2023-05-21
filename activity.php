a5782dd7e3f0dc8c64415f5bab3a980f7cf73706
Godly K Mathews <godlykmathews@outlook.com>
2023-05-21 22:27:07 +0530

    Activity

--- activity.php
+++ activity.php
@@ -54,12 +54,85 @@
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
               <div class="icon-box iconbox-blue">
                 <div class="icon">
-                  <img src="assets/logo/juniorredcrosslogo.webp" alt="nss" style="height: 110px; width: 110px; display: block;">
+                  <img src="assets/logo/juniorredcrosslogo.webp" alt="junior red cross" style="height: 110px; width: 110px; display: block;">
                 </div>
                 <h4>
                   Junior Red Cross
                 </h4>
                 <p>The Junior Red Cross was established in the hope of encouraging younger generations to cultivate their humanitarian spirit by understanding the Red Cross principles and ideas, and by participating actively in the Movement. The overarching idea behind the Junior Red Cross is to enable the country’s youth, so that they may one day contribute, in their own way, to the establishment of world peace and the welfare of Mankind.</p>
+              </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/literaryclublogo.webp" alt="literay club" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                Literary Club
+                </h4>
+                <p>Literary club plays a very important role in defining the subtle basics of language and culture in students. In today's world of electronic media, students are loosing their relation to the society and the natural world in general. Literary activities are very important in schools in this context. Students are given a large number of opportunities to develop their language and expression skills by means of activities and competitions, which extends to the surrounding societies and their issues.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/snehithalogo.webp" alt="snehitha" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                  Snehitha Gender Help Desk
+                </h4>
+                <p>Snehitha is a 24 hours working gender help desk. It works to avail support and help to the shield less women in society.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/womensforumlogo.webp" alt="women's forum" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                  Women's Forum
+                </h4>
+                <p>The Women's Forum in Kerala schools serves as a vital platform for empowering young women and promoting gender equality. It provides a safe space for students to voice their concerns, discuss pertinent issues, and advocate for their rights. Through engaging activities, workshops, and awareness campaigns, the forum helps cultivate a supportive and inclusive environment that fosters the growth and development of girls in school.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/artsportslogo.webp" alt="arts n sports" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                  Arts and Sports Club
+                </h4>
+                <p>The Arts and Sports Club in our school is a vibrant community where students can unleash their creative and athletic potential. It offers a diverse range of activities, including visual arts, performing arts, and a variety of sports disciplines. Through engaging workshops, competitions, and exhibitions, the club nurtures talent, promotes teamwork, and encourages students to explore their passions beyond the classroom.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/birdclublogo.webp" alt="snehitha" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                  Bird's Club
+                </h4>
+                <p>The Birds Club is a special group where young students observe and learn about different types of birds. They have the opportunity to watch birds in their natural habitats and discover interesting facts about them. The club encourages students to become keen observers and appreciate the beauty of birds around us.</p>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon-box iconbox-blue">
+                <div class="icon">
+                  <img src="assets/logo/ecoclublogo.webp" alt="snehitha" style="height: 110px; width: 110px; display: block;">
+                </div>
+                <h4>
+                  Eco Club
+                </h4>
+                <p>The Eco Club in our school promotes environmental awareness and sustainable practices through activities like tree planting and waste management campaigns. Students learn about the importance of taking care of the environment and are empowered to make a positive impact. The club encourages responsible actions and fosters a sense of environmental consciousness among its members.</p>
               </div>
             </div>
 
